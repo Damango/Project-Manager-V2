fde233be5d86896d8b3088e9e1d42c4a9408cc24
Damango <37273048+Damango@users.noreply.github.com>
2020-10-27 22:05:54 -0400

    Major Changes to the original files
    Reworked most of the initial developments. Many more Features. More to come.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import LandingPage from "./components/LandingPage/LandingPage"
+function App() {
+  return (
+    <div className="App">
+
+      <LandingPage />
+
+    </div>
+  );
+}
+
+export default App;
